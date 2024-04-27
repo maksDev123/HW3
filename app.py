ccf7b136e4732c05c3be7521d6398675683ccf51
maksDev123 <maksym.buleshnyi.pn@ucu.edu.ua>
2024-04-27 13:28:17 +0300

    Added tests

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
+
+TESTING = 1
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///users.sqlite3"
@@ -14,6 +17,9 @@ position_landmark_association = db.Table('position_landmarks',
 )
 
 user_logedin = None
+
+# Entities
+
 class Applicant(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(45))
@@ -46,8 +52,6 @@ class Offer(db.Model):
     position_id = db.Column(db.Integer, db.ForeignKey('position.id'), unique=False, nullable=False)
     position = db.relationship("Position", back_populates="offers")
 
-    # position_id = db.Column(db.Integer, db.ForeignKey('position.id'), unique=False, nullable=False)
-    # position = db.relationship("Position", back_populates="offers")
 
 
 class Landmark(db.Model):
@@ -86,8 +90,9 @@ class HR_Manager(db.Model):
     pos_level = db.Column(db.String(45))
     id_company = db.Column(db.Integer, db.ForeignKey('company.id'))
 
-    def __init__(self, first_name, last_name) -> None:
+    def __init__(self, first_name, last_name, pos_level) -> None:
         self.first_name = first_name
+        self.pos_level = pos_level
         self.last_name = last_name
 
 cv_universities_association = db.Table('cv_universities',
@@ -127,6 +132,46 @@ def initialize_universities():
         db.session.add(university)
 
     db.session.commit()
+
+
+# Queries
+@app.route('/applicants_with_applications', methods=['GET'])
+def get_applicants_with_applications():
+    sql_query = text("""
+        SELECT DISTINCT a.id, a.first_name, a.last_name
+        FROM Applicant a
+        JOIN Application app ON a.id = app.applicant_id
+    """)
+
+    result = db.session.execute(sql_query)
+    applicants_list = [{'id': row[0], 'first_name': row[1], 'last_name': row[2]} for row in result]
+    return jsonify(applicants_list)
+
+@app.route('/positions_with_three_applicants', methods=['GET'])
+def get_positions_with_three_applicants():
+    sql_query = text("""
+        SELECT p.id, p.title, p.salary
+        FROM Position p
+        JOIN Application app ON p.id = app.position_id
+        GROUP BY p.id
+        HAVING COUNT(app.id) >= 3
+    """)
+    result = db.session.execute(sql_query)
+    positions_list = [{'id': row[0], 'title': row[1], 'salary': row[2]} for row in result]
+    return jsonify(positions_list)
+
+
+@app.route('/applicants_with_offers', methods=['GET'])
+def get_applicants_with_offers():
+    sql_query = text("""
+        SELECT DISTINCT a.id, a.first_name, a.last_name
+        FROM Applicant a
+        JOIN Offer o ON a.id = o.applicant_id
+    """)
+    result = db.session.execute(sql_query)
+    applicants_list = [{'id': row[0], 'first_name': row[1], 'last_name': row[2]} for row in result]
+    return jsonify(applicants_list)
+
 
 @app.route('/', methods = ['GET', 'POST'])
 def main_page():
@@ -235,16 +280,6 @@ def update_recommendations(application_id):
     except Exception as e:
         return str(e), 500
 
-# @app.route('/applications/<int:application_id>', methods=['POST'])
-# def update_recommendations(application_id):
-
-#     recommendation = request.json['recommendation']
-#     application = Application.query.filter_by(id = application_id).first()
-#     application.recommedation = recommendation
-#     db.session.add(application)
-#     db.session.commit()
-#     return "Application received"
-
 
 @app.route('/positions/<int:position_id>', methods=['POST'])
 def handle_position_request(position_id):
@@ -309,10 +344,212 @@ def submit_form():
     user_logedin = applicant.id
     return render_template("main.html")
 
+
+
+
+
+def test_data_insertion():
+    numberApplicant = Applicant.query.count()
+    numberPosition = Position.query.count()
+    numberApplication = Application.query.count()
+    numberOffer = Offer.query.count()
+    numberCV = CV.query.count()
+    numberUniversity = University.query.count()
+    numberHRManager = HR_Manager.query.count()
+
+    applicant1 = Applicant(first_name='John', last_name='Doe')
+    db.session.add(applicant1)
+
+    position1 = Position(title='Software Engineer', salary=80000)
+    db.session.add(position1)
+
+    application1 = Application(recommendation='Good candidate')
+    application1.applicant = applicant1
+    application1.position = position1
+    db.session.add(application1)
+
+    offer1 = Offer(offered_salary=85000, accepted=1)
+    offer1.applicant = applicant1
+    offer1.position = position1
+    db.session.add(offer1)
+
+    cv1 = CV(experience='Bachelor in Computer Science')
+    db.session.add(cv1)
+
+    university1 = University(name='Cambridge', location='Cambridge')
+    university2 = University(name='Yale', location='New Haven')
+    db.session.add_all([university1, university2])
+
+    hr_manager1 = HR_Manager(first_name='Jane', last_name='Smith', pos_level='Senior')
+    db.session.add(hr_manager1)
+
+    db.session.commit()
+
+    assert Applicant.query.count() - numberApplicant == 1
+    assert Position.query.count() - numberPosition == 1
+    assert Application.query.count() - numberApplication == 1
+    assert Offer.query.count() - numberOffer == 1
+    assert CV.query.count() - numberCV == 1
+    assert University.query.count() - numberUniversity == 2
+    assert HR_Manager.query.count() - numberHRManager == 1
+
+
+def test_data_retrieval():
+    applicants = Applicant.query.all()
+    positions = Position.query.all()
+    applications = Application.query.all()
+    offers = Offer.query.all()
+
+    assert len(applicants) != 0
+    assert len(positions) != 0
+    assert len(applications) != 0
+    assert len(offers) != 0
+
+
+def test_data_modification():
+    application = Application.query.first()
+    application.recommendation = 'Excellent candidate'
+
+    offer = Offer.query.first()
+    offer.offered_salary = 90000
+
+    db.session.commit()
+    assert Application.query.filter_by(recommendation='Excellent candidate').first() is not None
+    assert Offer.query.filter_by(offered_salary=90000).first() is not None
+
+def test_data_deletion():
+    numberApplicant = Applicant.query.count()
+    numberOffer = Offer.query.count()
+    numberApplication = Application.query.count()
+    numberPosition = Position.query.count()
+    numberCV = CV.query.count()
+    numberUniversity = University.query.count()
+    numberHRManager = HR_Manager.query.count()
+
+    application = Application.query.first()
+    db.session.delete(application)
+
+    offer = Offer.query.first()
+    db.session.delete(offer)
+
+    applicant = Applicant.query.first()
+    db.session.delete(applicant)
+    
+    position = Position.query.first()
+    db.session.delete(position)
+    
+    cv = CV.query.first()
+    db.session.delete(cv)
+    
+    university = University.query.first()
+    db.session.delete(university)
+
+    manager = HR_Manager.query.first()
+    db.session.delete(manager)
+    
+    
+    db.session.commit()
+    assert numberApplicant - Applicant.query.count() == 1 
+    assert numberOffer - Offer.query.count() == 1
+    assert numberApplication - Application.query.count() == 1
+    assert numberPosition - Position.query.count() == 1 
+    assert numberCV - CV.query.count() == 1
+    assert numberUniversity - University.query.count() == 1 
+    assert numberHRManager - HR_Manager.query.count() == 1
+
+
+def data_integrity_testing():
+    try:
+        invalid_application = Application(recommendation='Good candidate')
+        db.session.add(invalid_application)
+
+        invalid_cv = CV(experience=None)
+        db.session.add(invalid_cv)
+
+        invalid_university = University(name=None, location='California')
+        db.session.add(invalid_university)
+
+        invalid_hr_manager = HR_Manager(first_name=None, last_name='Doe', pos_level='Manager')
+        db.session.add(invalid_hr_manager)
+
+        db.session.commit()
+    except Exception as e:
+        assert 'IntegrityError' in str(e)
+        db.session.rollback()
+
+def test_applicant():
+    num_applications_before = Application.query.count()
+
+    new_applicant = Applicant(first_name='John', last_name='Doe')
+    db.session.add(new_applicant)
+    db.session.commit()
+
+    new_position = Position(title='Software Engineer', salary=80000)
+    db.session.add(new_position)
+    db.session.commit()
+
+    application = Application(recommendation='Good candidate')
+    application.applicant = new_applicant
+    application.position = new_position
+    db.session.add(application)
+    db.session.commit()
+
+    num_applications_after = Application.query.count()
+
+    assert num_applications_after - num_applications_before == 1
+
+def test_hr_manager_actions():
+    new_applicant = Applicant(first_name='John', last_name='Doe')
+    db.session.add(new_applicant)
+    db.session.commit()
+
+    new_position = Position(title='Software Engineer', salary=80000)
+    db.session.add(new_position)
+    db.session.commit()
+
+    new_application = Application()
+    new_application.applicant = new_applicant
+    new_application.position = new_position
+    db.session.add(new_application)
+    db.session.commit()
+
+    new_hr_manager = HR_Manager(first_name='Jane', last_name='Smith', pos_level='Senior')
+    db.session.add(new_hr_manager)
+    db.session.commit()
+
+    new_application.recommendation = 'Highly recommended by HR'
+
+    assert new_application.recommendation == 'Highly recommended by HR'
+
+    new_offer = Offer(offered_salary=90000, accepted=0)
+    new_offer.applicant = new_applicant
+    new_offer.position = new_position
+    db.session.add(new_offer)
+    db.session.commit()
+
+    assert Offer.query.filter_by(applicant_id=new_applicant.id, position_id=new_position.id).first() is not None
+
+
+def run_tests():
+    test_data_insertion()
+    test_data_retrieval()
+    test_data_modification()
+    test_data_deletion()
+    data_integrity_testing()
+
+    # Applicant related test
+    test_applicant()
+
+    # HR Manager related test
+    test_hr_manager_actions()
+
+
 with app.app_context():
     db.create_all()
     initialize_universities()
     initialize_landmark()
+    if TESTING:
+        run_tests()
 
 if __name__ == "__main__":
     app.run(port=4000,debug=True)
